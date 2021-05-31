9e16686681dc3a48518617875821e80c3ab1f314
Seongeon Park <goopse3928@gmail.com>
2021-05-31 16:21:00 +0900

    Update 'Animate Needle' script

--- Assets/_Scripts/_Charts/Drone_Scenario/AnimateNeedle.cs
+++ Assets/_Scripts/_Charts/Drone_Scenario/AnimateNeedle.cs
@@ -9,10 +9,14 @@ public class AnimateNeedle : MonoBehaviour
     [Header("UI:")]
     [SerializeField] private RectTransform _Needle;
     [SerializeField] private float _NeedleSpeed;
-    [SerializeField] private float _MaxAngle;
-    [SerializeField] private float _MinAngle;
+    [SerializeField] private float _AngleOfStart;
+    [SerializeField] private float _AngleOfEnd;
+    [SerializeField] private float _MinValue;
+    [SerializeField] private float _MaxValue;
     [Header("Data:")]
     [SerializeField] private float _RefreshTime;
+    [Space]
+    [SerializeField] private float _TestValue;
 
 
     #endregion  // Inspector_Window_Variables
@@ -20,34 +24,55 @@ public class AnimateNeedle : MonoBehaviour
 
     #region PRIVATE_VARIABLES
 
+    private float mServerData = 0f;
     private float mNeedleValue = 0f;
 
     #endregion  // PRIVATE_VARIABLES
 
 
     #region UNITY_MONOBEHAVIOUR_METHODS
-    
-    private void Update()
+
+    private void Awake()
     {
-        if (_MinAngle <= mNeedleValue && mNeedleValue <= _MaxAngle)
-        {
-            mNeedleValue += _NeedleSpeed * Time.deltaTime;
-        }
+        mServerData = _TestValue;
     }
 
+    private void Start()
+    {
+        StartCoroutine("UpdateGraph");
+    }
 
+    private void Update()
+    {
+        mNeedleValue = Mathf.Clamp(mNeedleValue, _MinValue, _MaxValue);
+
+        if (_MinValue <= mNeedleValue && mNeedleValue <= mServerData)
+        {
+            mNeedleValue += _NeedleSpeed * Time.deltaTime;
+            Debug.Log(mNeedleValue);
+
+            _Needle.eulerAngles = new Vector3(0, 0, GetRotationOfValue());
+        }
+    }
 
     #endregion  // UNITY_MONOBEHAVIOUR_METHODS
 
 
     #region PRIVATE_METHODS
 
+    private float GetRotationOfValue()
+    {
+        float totalAngleSize = Mathf.Abs(_AngleOfStart - _AngleOfEnd);
+        float valueNormalized = mNeedleValue / _MaxValue;
+
+        return _AngleOfStart - (valueNormalized * totalAngleSize);
+    }
 
     private IEnumerator UpdateGraph()
     {
         while (true)
         {
-
+            mNeedleValue = 0f;
 
             yield return new WaitForSeconds(_RefreshTime);
         }
