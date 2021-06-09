614adbdf38f06322939b8ac10bf5e082cbb91747
Seongeon Park <goopse3928@gmail.com>
2021-06-09 17:19:32 +0900

    [ADD] 드론 app bar 관련 코드 추가

---
+++ Assets/_Scripts/_System/Drone_AppBar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Drone_AppBar : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _ActivationTimeTMP;
+    [SerializeField] private TextMeshProUGUI _DateTimeTMP;
+
+    private DateTime mStartTime;
+
+    private void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        mStartTime = DateTime.Now;
+    }
+
+    void Update()
+    {
+        
+        TimeSpan timeDiff = DateTime.Now - mStartTime;
+        _ActivationTimeTMP.text = string.Format("Elapsed time {0:mm\\:ss}", timeDiff);
+        _DateTimeTMP.text = DateTime.Now.ToString("yyyy/MM/dd   HH:mm:ss");
+    }
+}
