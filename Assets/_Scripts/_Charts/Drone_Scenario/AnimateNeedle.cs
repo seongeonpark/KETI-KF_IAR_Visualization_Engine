c2dde0b818995324557d4978a08eccc526f41eb2
Seongeon Park <goopse3928@gmail.com>
2021-06-24 17:44:01 +0900

    [UPDATE] parser의 ArgumentOutOfRangeException 해결

--- Assets/_Scripts/_Charts/Drone_Scenario/AnimateNeedle.cs
+++ Assets/_Scripts/_Charts/Drone_Scenario/AnimateNeedle.cs
@@ -1,23 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
+
+public enum EChartType
+{
+    Battery_remain,
+    Airspeed,
+    HeadingIndicator,
+    TurnCoordinator,
+    Attitude,
+    Battery_voltage
+}
 
 public class AnimateNeedle : MonoBehaviour
 {
     #region Inspector_Window_Variables
+    [SerializeField] private EChartType _ChartType;
 
     [Header("UI:")]
     [SerializeField] private RectTransform _Needle;
     [SerializeField] private float _NeedleSpeed;
+
+    [Space()]
     [SerializeField] private float _AngleOfStart;
     [SerializeField] private float _AngleOfEnd;
+
+    [Space()]
     [SerializeField] private float _MinValue;
     [SerializeField] private float _MaxValue;
+
     [Space()]
     [SerializeField] private TextMeshProUGUI _Text;
+
     [Header("Data:")]
     [SerializeField] private GameObject _Http_Parser;
     [SerializeField] private float _RefreshTime;
+
     [Header("Test:")]
     [SerializeField] private bool _IsTest;
     [SerializeField] private float _TestValue;
@@ -27,62 +46,53 @@ public class AnimateNeedle : MonoBehaviour
 
     #region PRIVATE_VARIABLES
 
-    private HTTP_Parser_v01 mParser;
+    private HTTP_Parser_v01 m_Parser;
+    private float m_ServerData = 0f;
+    private float m_NeedleValue = 0f;
+    private float m_PreNeedleValue = 0f;
+    private float m_CurrentNeedleValue = 0f;
 
-    private float mServerData = 0f;
-    private float mNeedleValue = 0f;
-    private float mPreNeedleValue = 0f;
-    private float mCurrentNeedleValue = 0f;
-    
+    private bool m_IsReady = false;
+
     #endregion  // PRIVATE_VARIABLES
-        
+
 
     #region UNITY_MONOBEHAVIOUR_METHODS
 
+    private void Awake()
+    {
+        m_Parser = _Http_Parser.GetComponent<HTTP_Parser_v01>();
+    }
+
     private void Start()
     {
-        mParser = _Http_Parser.GetComponent<HTTP_Parser_v01>();
-        mCurrentNeedleValue = mServerData;
-
         StartCoroutine("UpdateGraph");
-        
     }
 
     private void Update()
     {
-        //mServerData = mParser._out_pitch[0] * 10000.0f;
-        //Debug.Log("server data: " + mServerData);
-
-        if (_IsTest)    // make test value
+        if (!m_IsReady)
         {
-            _TestValue = Random.Range(0, 360);
-            mServerData = _TestValue;
+            m_IsReady = isReady(_ChartType);
         }
 
-        mNeedleValue = Mathf.Clamp(mServerData, _MinValue, _MaxValue);
-        Debug.Log("needle value: " + mNeedleValue);
-
-
-        // Animation
-        if (mPreNeedleValue <= mNeedleValue && mNeedleValue < mCurrentNeedleValue)
+        // # 3. Animation
+        if (m_PreNeedleValue <= m_NeedleValue && m_NeedleValue < m_CurrentNeedleValue)
         {
-            mNeedleValue += (_NeedleSpeed * Time.deltaTime);
-
+            m_NeedleValue += _NeedleSpeed * Time.deltaTime;
+            _Needle.eulerAngles = new Vector3(0, 0, GetRotation(m_NeedleValue));
         }
-        else if (mPreNeedleValue >= mNeedleValue && mNeedleValue > mCurrentNeedleValue)
+        else if (m_CurrentNeedleValue < m_NeedleValue && m_NeedleValue <= m_PreNeedleValue)
         {
-            mNeedleValue -= (_NeedleSpeed * Time.deltaTime);
-            Debug.Log("animation needle value: " + mNeedleValue);
-
+            m_NeedleValue -= _NeedleSpeed * Time.deltaTime;
+            _Needle.eulerAngles = new Vector3(0, 0, GetRotation(m_NeedleValue));
         }
         else
         {
-            mNeedleValue = mCurrentNeedleValue;
+            m_NeedleValue = m_CurrentNeedleValue;
+            _Needle.eulerAngles = new Vector3(0, 0, GetRotation(m_NeedleValue));
         }
 
-        _Needle.eulerAngles = new Vector3(0, 0, GetRotationOfValue(mNeedleValue));
-
-        
     }
 
     #endregion  // UNITY_MONOBEHAVIOUR_METHODS
@@ -90,26 +100,119 @@ public class AnimateNeedle : MonoBehaviour
 
     #region PRIVATE_METHODS
 
-    private float GetRotationOfValue(float value)
+    private float GetRotation(float value)
     {
         float totalAngleSize = Mathf.Abs(_AngleOfStart - _AngleOfEnd);
         float valueNormalized = value / _MaxValue;
 
         return _AngleOfStart - (valueNormalized * totalAngleSize);
     }
+    
+    private float GetParserData(EChartType chart)
+    {
+        float data;
+
+        switch (chart)
+        {
+            case EChartType.Battery_remain:
+                data = m_Parser._out_remainbattery[0];
+                break;
+            case EChartType.Airspeed:
+                data = m_Parser._out_airspeed[0];
+                break;
+            case EChartType.HeadingIndicator:
+                data = m_Parser._out_yaw[0];
+                break;
+            case EChartType.TurnCoordinator:
+            //    data = m_Parser._out_roll[0];
+            //    data = m_Parser._out_pitch[0];
+            case EChartType.Attitude:
+            //    data = m_Parser._out_roll[0];
+            //    data = m_Parser._out_pitch[0];
+            case EChartType.Battery_voltage:
+                data = m_Parser._out_currentbattery[0];
+                break;
+            default:
+                data = 0;
+                break;
+        }
+
+        return Convert.ToSingle(data);
+    }
+
+    private bool isReady(EChartType chart)
+    {
+        switch (chart)
+        {
+            case EChartType.Battery_remain:
+                if (0 < m_Parser._out_remainbattery.Count)
+                {
+                    return true;
+                }
+                else return false;
+            case EChartType.Airspeed:
+                if (0 < m_Parser._out_airspeed.Count)
+                {
+                    return true;
+                }
+                else return false;
+            case EChartType.HeadingIndicator:
+                if (0 < m_Parser._out_yaw.Count)
+                {
+                    return true;
+                }
+                else return false;
+            case EChartType.TurnCoordinator:
+                //if (0 <
+                //    return true;
+                //}
+                //else return false;
+            case EChartType.Attitude:
+                //if (0 < m_Parser._out_pitch.Count && 0 < m_Parser._out_roll.Count)
+                //{
+                //    return true;
+                //}
+                //else return false;
+            case EChartType.Battery_voltage:
+                if (0 < m_Parser._out_currentbattery.Count)
+                {
+                    return true;
+                }
+                else return false;
+            default:
+                break;
+        }
+        return false;
+    }
 
     private IEnumerator UpdateGraph()
     {
         while (true)
         {
-            mPreNeedleValue = mCurrentNeedleValue;
-            mCurrentNeedleValue = mServerData;
+            if (_IsTest)    // make test value
+            {
+                //_TestValue = Random.Range(0, 360);
+                m_ServerData = _TestValue;
+            }
+            if (!_IsTest && m_IsReady)
+            {
+                // # 1. Get data from SERVER
+                m_ServerData = GetParserData(_ChartType);
+            }
 
+            // # 2. Pre-processing
+            // .... normalization
+            m_CurrentNeedleValue = Mathf.Clamp(m_ServerData, _MinValue, _MaxValue);
+
+            // # 2. Pre-processing
+            // .... config
+            m_PreNeedleValue = m_CurrentNeedleValue;
+            m_NeedleValue = m_PreNeedleValue;
 
             // Text indicator
             if (_Text != null)
             {
-                _Text.text = string.Format("{0} %", mServerData);
+                _Text.text = string.Format("{0} %", m_ServerData);
             }
 
             yield return new WaitForSeconds(_RefreshTime);
